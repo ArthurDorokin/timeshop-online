111db95d768d905d6baa902a1d80c52c0c24a6c2
ArthurDorokhin <a.u.dorokhin@gmail.com>
2020-08-04 10:13:20 +0300

    project time shop online

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -6,7 +6,28 @@ class Header extends Component {
         return (
             <div className="header">
                 <div className="container">
-                    <p>header</p>
+                    <div className="wrap">
+                        <div className="logo">
+                            <img src="../img/25529133563025_small2.jpg" alt=""/>
+                        </div>
+                        <div className="navigation">
+                            <ul>
+                                <li><a href="#!">Мужские</a></li>
+                                <li><a href="#!">Женские</a></li>
+                                <li><a href="#!">Детские</a></li>
+                                <li><a href="#!">Аксессуары</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="basket">
+                        <ul>
+                            <li className="img">
+                                <span className="count-num">0</span>
+                                <img src="../img/vippng.com-luggage-icon-png-2246050.png" alt=""/>
+                            </li>
+                            <li>Мой заказ</li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         )
