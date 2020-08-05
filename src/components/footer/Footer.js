474544eb0c32e080ea3f2a056058022a0da4f7e7
ArthurDorokhin <a.u.dorokhin@gmail.com>
2020-08-05 19:28:55 +0300

    project time shop online

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -6,11 +6,32 @@ class Footer extends Component {
         return (
             <div className="footer">
                 <div className="container">
-                    <p>footer</p>
+                    <div className="img-logo">
+                        <a href="/"><img src="../img/25529133563025_small2.jpg" alt=""/></a>
+                    </div>
+                    <div className="footer-nav">
+                        <div className="catalog">
+                            <ul>
+                                <li>Каталог</li>
+                                <li><a href="/">Мужские</a></li>
+                                <li><a href="/">Женские</a></li>
+                                <li><a href="/">Детские</a></li>
+                            </ul>
+                        </div>
+                        <div className="contact">
+                            <ul>
+                                <li>Клиентам</li>
+                                <li><a href="/">044 000-00-00</a></li>
+                                <li><a href="/">090 000-00-00</a></li>
+                                <li><a href="/">test@test.com</a></li>
+                                <li><a href="/">Киев</a></li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
             </div>
         )
     }
-};
+}
 
 export default Footer
