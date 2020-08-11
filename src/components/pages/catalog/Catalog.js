423a52f967c3ecee9106d3e74ec7a3d2e3f74848
ArthurDorokhin <a.u.dorokhin@gmail.com>
2020-08-12 00:59:04 +0300

    project time shop online

--- src/components/pages/catalog/Catalog.js
+++ src/components/pages/catalog/Catalog.js
@@ -1,15 +1,43 @@
 import React, {Component} from "react";
+import Filter from "../filter";
+import StoreContext from "../../../StoreContext";
+import {NavLink} from "react-router-dom";
 
-class Filter extends Component {
+class Catalog extends Component {
     render() {
         return(
-            <div className="catalog">
-                <div className="container">
-                    catalog
+            <div className="content in">
+                <div className="block-catalog">
+                    <div className="container">
+                        <h1>Каталог</h1>
+                        <img src="../img/52324689631877.jpg" alt="catalog"/>
+                        <div className="wrap-aksessuary wrap-internal-card">
+                            <Filter/>
+                            <div className="blocks-aksessuary blocks-internal-card">
+                                <StoreContext.Consumer>
+                                    {(catalog) => (
+                                        catalog.map((item) =>
+                                            <div className="aksessuary-item internal-card-item" key={item.id}>
+                                                <div className="aksessuary-blocks _card-item">
+                                                    <NavLink to={item.link}>
+                                                        <img src={item.img} alt={item.alt}/>
+                                                        <span className="flag">{item.filterIcon.name}</span>
+                                                        <p className="description">{item.description}</p>
+                                                        <p className="price">{item.price} грн</p>
+                                                    </NavLink>
+                                                </div>
+                                                <div className="btn-price">{item.buy}</div>
+                                            </div>
+                                        )
+                                    )}
+                                </StoreContext.Consumer>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         )
     }
 }
 
-export default Filter
+export default Catalog
